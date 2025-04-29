e8115e116ed3fb5b2f669dd461327c06ef385542
FacundoVillarroel <facu.villarroel96@gmail.com>
2025-04-29 18:07:19 -0400

    [logger] - adapt logger to not write on the disk in vercel or prod

--- api/src/logs/logger.js
+++ api/src/logs/logger.js
@@ -1,20 +1,47 @@
 const log4js = require("log4js");
 
-log4js.configure({
-  appenders:{
-    console: {type: "console"},
-    warnFile: { type: "file", filename: "./src/logs/warn.log"},
-    errorFile: { type: "file", filename: "./src/logs/error.log"},
-    soloWarn: {type: "logLevelFilter", appender: "warnFile", level: "warn"},
-    soloError: {type: "logLevelFilter", appender: "errorFile", level: "error"},
-  },
-  categories:{ 
-    default: {appenders: ["console"], level: "info"},
-    development: { appenders: ["console", "soloWarn", "soloError"], level: "info" },
-    production: { appenders: ["soloWarn", "soloError"], level: "warn"}
-  }
-})
+const isVercel =
+  process.env.VERCEL === "1" || process.env.NODE_ENV === "production";
 
-const logToUse = process.env.NODE_ENV === "PROD" ? "production" : "development";
+const appenders = {
+  console: { type: "console" },
+};
+
+const categories = {
+  default: { appenders: ["console"], level: "info" },
+};
+
+if (!isVercel) {
+  appenders.warnFile = { type: "file", filename: "./src/logs/warn.log" };
+  appenders.errorFile = { type: "file", filename: "./src/logs/error.log" };
+  appenders.soloWarn = {
+    type: "logLevelFilter",
+    appender: "warnFile",
+    level: "warn",
+  };
+  appenders.soloError = {
+    type: "logLevelFilter",
+    appender: "errorFile",
+    level: "error",
+  };
+
+  categories.development = {
+    appenders: ["console", "soloWarn", "soloError"],
+    level: "info",
+  };
+  categories.production = {
+    appenders: ["soloWarn", "soloError"],
+    level: "warn",
+  };
+}
+
+log4js.configure({ appenders, categories });
+
+// Use "development" unless you're in Vercel or production
+const logToUse = isVercel
+  ? "default" // Console-only in Vercel
+  : process.env.NODE_ENV === "PROD"
+  ? "production"
+  : "development";
 
 module.exports = log4js.getLogger(logToUse);
